1d0687779d9b8babfb34674c79e9a85ef6b9e706
sbmansfield <sbmansfield@NN123-HA-11.HS.sd203.org>
2015-09-10 14:16:18 -0500

    javadocumentation

---
+++ Chapter 3 Practice/Car.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * A car has a certain fuel efficiency and a certain amount of fuel in the gas tank.
+ * The car may be driven, which reduces the amount of gas in the fuel tank.
+ * 
+ * @author Sarah Mansfield 
+ * @version 10 September 2015
+ */
+public class Car
+{
+    /** the fuel efficiency for the car measured in units of miles/gallon (mpg) */
+    private double fuelEfficiency;
+    
+    /** the amount of fuel in the tank of the car measured in units of gallons */
+    private double fuelInTank;
+   
+    /**
+     * Constructor for objects of class Car that specifies the fuel efficiency
+     */
+    public Car(double fuelEfficiency)
+    {
+        // initialise instance variables
+        fuelEfficiency = fuelEfficiency;
+        fuelInTank = 0;
+    }
+
+    /**
+     * This method simulates driving the car for the specified distance and 
+     * reduces the amount of gas in the fuel tank
+     *
+     * @pre     the specified distance will not consume more than the available gas
+     * @param    distance    the specified distance to drive in units of miles
+     */
+    public void drive(double distance)
+    {
+        // put your code here
+        return x+y;
+    }
+
+}
