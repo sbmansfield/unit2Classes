6a40e8deb471aa4ca022b047cd14726dff35eb8f
sbmansfield <sbmansfield@NN123-HA-11.HS.sd203.org>
2015-09-15 14:11:21 -0500

    door program

---
+++ Chapter 3 Practice/Door.java
@@ -0,0 +1,109 @@
+
+
+/**
+ * Write a description of class Door here.
+ * 
+ * @author Sarah Mansfield
+ * @version 15 September 2015
+ */
+public class Door
+{
+    /** the description of the door (name) */
+    private String name;
+    
+    /** the state of the door (open/closed) */
+    private String state;
+
+    /**
+     * Default constructor for objects of class Door
+     */
+    public Door(String name, String state)
+    {
+        // initialise instance variables
+        this.name = "Front";
+        this.state = "Open";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public String open(String state)
+    {
+       this.state = "Open";
+       return state;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String close(String state)
+    {
+        this.state = "Closed";
+        return state;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getState()
+    {
+        return this.state;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
+
+}
